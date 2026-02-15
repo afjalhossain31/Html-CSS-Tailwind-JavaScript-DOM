0b1c5f328b0fd7496ad95651c02783d1cd68fee8
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-02-15 17:13:35 +0600

    add getelementbyId or queryselectorAll

---
+++ js/CssJs.js
@@ -0,0 +1,24 @@
+console.log("connected to the my css file using javascript");
+
+// const section =document.getElementById("players-container");
+const sections= document.querySelectorAll("section"); 
+// console.log(sections);
+for(const section of sections){
+    console.log(section);
+    section.style.backgroundColor="lightblue";
+    section.style.border="2px solid green";
+    section.style.padding="10px";
+    section.style.margin="20px";
+    section.style.borderRadius="10px";
+    section.style.boxShadow="0 4px 8px rgba(0, 0, 0, 0.2)";
+}
+
+// css er modde style add kore just kon section niye kaj korte chai oita dite hobe tahole oi oita kaj korbe 
+// for( const section of sections){
+//     section.classList.add("player-section");}
+
+
+
+
+
+
