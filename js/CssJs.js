e4ea1073ad502a4ba48916e142e7dcf44bcbc1e2
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-02-18 19:44:51 +0600

    added styling dom properties ,add remove css

--- js/CssJs.js
+++ js/CssJs.js
@@ -1,6 +1,7 @@
 console.log("connected to the my css file using javascript");
 
 // const section =document.getElementById("players-container");
+// id dile # dite hobe r class dile . dite hobe
 const sections= document.querySelectorAll("section"); 
 // console.log(sections);
 for(const section of sections){
@@ -11,6 +12,7 @@ for(const section of sections){
     section.style.margin="20px";
     section.style.borderRadius="10px";
     section.style.boxShadow="0 4px 8px rgba(0, 0, 0, 0.2)";
+    section.style.fontFamily="Arial, sans-serif";
 }
 
 // css er modde style add kore just kon section niye kaj korte chai oita dite hobe tahole oi oita kaj korbe 
