6df6dad6397d348559f92d0606cd56ca31acf854
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-03-10 16:51:42 +0600

    set and get Object to local storage

---
+++ js/browser.js
@@ -0,0 +1,36 @@
+console.log('Hello World');
+
+function addNumberTolS() {
+    const num = Math.ceil(Math.random() * 100);
+    console.log( num);
+    localStorage.setItem('number', num);
+}
+
+function setObjectTolS() {
+    const customer = {
+        name: 'Afjal',
+        product : 'Laptop',
+        price: 50000
+    };
+
+    const customerJSON = JSON.stringify(customer);
+    localStorage.setItem('customer', customerJSON);
+}
+
+function getCustomerFromls() {
+    const customerJSON = localStorage.getItem('customer');
+    const customer = JSON.parse(customerJSON);
+    console.log(customer);
+}
+
+function getNumberFromls() {
+    const num = localStorage.getItem('number');
+    console.log(num);
+}
+
+function readObjectFromLS() {
+    const customerJSON = localStorage.getItem('customer');
+    const customer = JSON.parse(customerJSON);
+    console.log(customer.name);
+}
+
